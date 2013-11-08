75bf3a7aafa5fb193a2798b468506e82f8b6f38b
Marslo <marslo.jiao@gmail.com>
2013-11-08 13:11:38 +0200

    Add mycancan

---
+++ mycancan/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mycancan::Application.config.secret_token = '5dd8ec74977e0648c44996caabede8411d6d105b3f1edfc18eb384c37f031aacd5cf93b44093e5e94d912eb5b975e0d4ff2b6851465d23d06a83905d56a57596'
