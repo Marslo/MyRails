5a1f9b64aa061ed36d563927f89e05ed445a4032
Marslo <marslo.jiao@gmail.com>
2013-11-09 16:02:45 +0200

    Update routes.rb for authenticated

--- mycancan/config/routes.rb
+++ mycancan/config/routes.rb
@@ -1,6 +1,9 @@
 Mycancan::Application.routes.draw do
-  get "home/index"
-
+  # get "home/index"
+  authenticated :user do
+    root :to => 'home#index'
+  end
+  root :to => 'home#index'
   devise_for :users
 
   # The priority is based upon order of creation:
