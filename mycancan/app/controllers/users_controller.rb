a88dea3754a783d41e5677c11c289bf6c519c145
Marslo <marslo.jiao@gmail.com>
2013-11-11 09:22:42 +0200

    User Contorller with CanCan 'load_and_authorize_resource'

--- mycancan/app/controllers/users_controller.rb
+++ mycancan/app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
+  load_and_authorize_resource :only => :index
+
   def index
     authorize! :index, @user, :message => "Not authorized as a administrator"
     @user = User.all
