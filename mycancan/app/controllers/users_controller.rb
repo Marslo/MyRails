6dd0afbf08a735b5248cd6869936b974809835d2
Marslo <marslo.jiao@gmail.com>
2013-11-11 11:16:44 +0200

    Add udpate and destory action to the users contorller

--- mycancan/app/controllers/users_controller.rb
+++ mycancan/app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
 
+  def index
+    authorize! :index, @user, :message => 'Not authorized as an administrator.'
+    @users = User.all
+  end
+
   # User controller with CanCan "load_and_authorize_resource"
   load_and_authorize_resource :only => :index
   # def index
@@ -20,5 +25,26 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def update
+    authorize! :update, @user, :message => 'Not authorized as an administrator.'
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user], :as => :admin)
+      redirect_to users_path, :notice => 'User updated.'
+    else
+      redirect_to user_path, :alert => 'Unable to update user.'
+    end
+  end
+
+  def destory
+    authorized! :destory, @user, :message => 'Not authorized as an administrator.'
+    user = User.find(params[:id])
+    unless user == current_user
+      user.destory
+      redirect_to user_path, :notice => 'User deleted.'
+    else
+      redirect_to user_path, :notice => 'Cannot delete yourself.'
+    end
   end
 end
