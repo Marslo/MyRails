d45e09219c886919f5aa7818697655c7357503b3
Marslo <marslo.jiao@gmail.com>
2013-11-11 09:20:00 +0200

    User controller with CanCan '@authorize\!'

--- mycancan/app/controllers/users_controller.rb
+++ mycancan/app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
+  before_filter :authenticate_user!
   def index
+    authorize! :index, @user, :message => "Not authorized as a administrator"
+    @user = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 end
