9c1b7291d63f7194623991883fde0ed508ea08ec
Marslo <marslo.jiao@gmail.com>
2013-11-11 10:15:37 +0200

    Add the User controller without CanCan (all commented)

--- mycancan/app/controllers/users_controller.rb
+++ mycancan/app/controllers/users_controller.rb
@@ -1,11 +1,22 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
-  load_and_authorize_resource :only => :index
 
-  def index
-    authorize! :index, @user, :message => "Not authorized as a administrator"
-    @user = User.all
-  end
+  # User controller with CanCan "load_and_authorize_resource"
+  load_and_authorize_resource :only => :index
+  # def index
+    # authorize! :index, @user, :message => "Not authorized as a administrator"
+    # @user = User.all
+  # end
+
+  # Users controller without cancan (Provided by Rolify gem)
+  # before_filter :only_allow_admin, :only => [:index]
+  # def index
+    # @users = User.all
+  # end
+  # private
+  # def only_allow_admin
+    # redirect_to root_path, :alert => 'Not authorized as an administrator.' unless current_user.has_role? :admin
+  # end
 
   def show
     @user = User.find(params[:id])
